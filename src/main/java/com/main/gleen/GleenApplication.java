4d5b669ebaabed3c3141c58f0de9bca4146a46f3
Aristide Fumo <aristide.fumo@sopth.com>
2022-10-17 23:39:06 +0200

    maven configure mongodb, springboot and other

---
+++ src/main/java/com/main/gleen/GleenApplication.java
@@ -0,0 +1,13 @@
+package com.main.gleen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GleenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GleenApplication.class, args);
+	}
+
+}

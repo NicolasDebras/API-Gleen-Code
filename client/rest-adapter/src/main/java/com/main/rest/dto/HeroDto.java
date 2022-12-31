b9efc4b6025b23755c4f42e2aeea0d635d55d182
Aristide Fumo <aristide.fumo@sopth.com>
2022-12-31 16:15:27 +0100

    error

--- client/rest-adapter/src/main/java/com/main/rest/dto/HeroDto.java
+++ client/rest-adapter/src/main/java/com/main/rest/dto/HeroDto.java
@@ -3,6 +3,8 @@ package com.main.rest.dto;
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.main.domain.functionnal.model.RarityHero;
 import com.main.domain.functionnal.model.SpecialityHero;
+import lombok.Builder;
+
 import java.util.UUID;
 
 import static com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility.ANY;

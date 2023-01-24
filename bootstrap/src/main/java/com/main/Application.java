f4c39c94e12af6f2b60155dff37b7e9f455f491a
Aristide Fumo <aristide.fumo@sopth.com>
2023-01-24 22:42:48 +0100

    add swagger for documentation

--- bootstrap/src/main/java/com/main/Application.java
+++ bootstrap/src/main/java/com/main/Application.java
@@ -6,10 +6,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Import;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Import({ApplicationConfiguration.class})
 @SpringBootApplication(scanBasePackages = "com.main")
 @EnableMongock
+@EnableSwagger2
 @EntityScan("com.main")
 public class Application {
 

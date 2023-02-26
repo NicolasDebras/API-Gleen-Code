45b6e0c8b4687b5790a0723cd5f1c7f271777076
Aristide Fumo <aristide.ff@gmail.com>
2023-02-26 19:51:17 +0100

    solve error HttpMediaTypeNotAcceptableException

--- client/rest-adapter/src/main/java/com/main/dto/PackDto.java
+++ client/rest-adapter/src/main/java/com/main/dto/PackDto.java
@@ -1,7 +1,12 @@
 package com.main.dto;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+
 import java.util.List;
 
+import static com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility.ANY;
+
+@JsonAutoDetect(fieldVisibility = ANY)
 public record PackDto(
 
         List<CardDto> cards
